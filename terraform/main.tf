04951221f7a305bb64fe8166ec4cea30a0693993
vjrmalla <67913193+vjrmalla@users.noreply.github.com>
2021-02-19 18:07:28 +0000

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,7 +2,7 @@
 terraform{
     backend "s3"{
         # Replace this with your bucket name!
-        bucket         = "terraform-up-and-running-state"
+        bucket         = "mvjr-terraform-running-state"
         key            = "global/s3/terraform.tfstate"
         region         = "eu-west-2"
 
@@ -18,7 +18,7 @@ provider "aws" {
 }
 
 resource "aws_s3_bucket" "terraform_state"{
-    bucket= "terraform-up-and-running-state"
+    bucket= "mvjr-terraform-running-state"
     #region="eu-west-2"
     # Prevent accidental deletion of this bucket
     lifecycle{
